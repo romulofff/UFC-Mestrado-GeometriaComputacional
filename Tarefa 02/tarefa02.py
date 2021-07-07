ad113ab445ebf5f19376fb9d2941d61f16927a49
romulofff <romulofffufc@gmail.com>
2021-07-07 16:26:18 -0300

    Add new ops

--- Tarefa 02/tarefa02.py
+++ Tarefa 02/tarefa02.py
@@ -19,7 +19,7 @@ def prodescalar(x, y):
     z = []
     for i in range(len(x)):
         z.append(x[i]*y[i])
-    return z
+    return sum(z)
 
 
 def norma(x):
@@ -29,7 +29,20 @@ def norma(x):
     return math.sqrt(z)
 
 
+def distancia(x, y):
+    z = []
+    for i in range(len(x)):
+        z.append(x[i]-y[i])
+    return norma(z)
+
+
+def angulo(x, y):
+    return math.acos(prodescalar(x, y)/(norma(x)*norma(y)))*(180/math.pi)
+
+
 print(somavetorial([0, 2, 3], [1, 2, 0]))
 print(multescalar(2, [1, 1]))
 print(prodescalar([1, 2, 3], [3, 2, 1]))
 print(norma([1, 2]))
+print(distancia([3, 3], [0, 0]))
+print(angulo([2, -4, -1], [0, 5, 2]))
